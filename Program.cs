a561f4292600d4ed2d2e9feec80fd2cf08dff00e
liber <avan4315@163.com>
2021-02-26 14:38:45 +0800

    list todo

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace ori_and_the_blind_forest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //TODO: GameFlow
+            /* 
+             * Find Tree -> Skill
+             * Get Ability Cells,Spirit Light Container & Spirit Light -> Learn Ability
+             */
+            //TODO: Skill
+            //TODO: Ability Cells
+            //TODO: Spirit Light Container
+            //TODO: Spirit Light
+            //TODO: Learn Ability
+
+            LearnSkill();
+        }
+        static void LearnSkill()
+        {
+            // acquire skill
+        }
+        static void AbilityCells()
+        {
+            // get 1 cell
+        }
+        static void SpiritLightContainer()
+        {
+            // get 1 spiritLightContainer
+        }
+        static void SpiritLight()
+        {
+            // kill 1 animal
+            // pick spiritLight
+        }
+        static void LearnAbility()
+        {
+            // enough ability point
         }
     }
 }
