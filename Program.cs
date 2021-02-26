7be0c717c15944b8467fe58f543ea50e5f8728f1
liber <avan4315@163.com>
2021-02-26 10:24:46 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ori_and_the_blind_forest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
